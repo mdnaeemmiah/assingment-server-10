3927bc5a2ecf8105df727e6d29406f59572da9eb
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2024-09-01 12:40:31 +0600

    project is end

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
-// middleware
+
+// Middleware
 // app.use(
 //   cors({
-//     origin: ["http://localhost:5173","http://localhost:5174",'https://module-52-dragon-3792f.web.app','https://module-52-dragon-3792f.firebaseapp.com'],
+//     origin: [
+//       'https://hilarious-gnome-f40639.netlify.app',
+//     ],
 //   })
 // );
-app.use(cors());
+app.use(cors())
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qenm5ah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -28,30 +29,30 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     // await client.connect();
-    const userCollection = client.db("craftDB").collection("craft");
-    const userEmailPass = client.db("userDB").collection("user");
+    const userCollection = client.db("Art-Craft").collection("craft");
+    const userEmailPass = client.db("Art-Craft").collection("user");
 
     app.get('/user', async (req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
     app.get('/user/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.findOne(query);
       res.send(result);
-    })
+    });
 
     app.post('/user', async (req, res) => {
       const newUser = req.body;
       console.log('new user', newUser);
       const result = await userCollection.insertOne(newUser);
       res.send(result);
-    })
+    });
 
     app.put('/user/:id', async (req, res) => {
       const id = req.params.id;
@@ -70,39 +71,38 @@ async function run() {
           email: user.email,
           photo: user.photo
         }
-      }
+      };
       const result = await userCollection.updateOne(filter, updateUser, option);
       res.send(result);
-    })
+    });
 
     app.delete('/user/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
-    // for users email and name
-    app.get('/user',async(req,res)=>{
+    // For users email and name
+    app.get('/user', async (req, res) => {
       const query = userEmailPass.find();
       const result = await query.toArray();
       res.send(result);
-    })
+    });
 
-    app.post('/user',async(req,res)=>{
-      const newUser= req.body;
-      console.log('new user',newUser);
-      const result = await userEmailPass.insertOne(newUser)
-      res.send(result)
-    })
-    app.delete('/user/:id',async(req,res)=>{
-      const id =req.params.id;
-      const query = {_id : new ObjectId(id)}
+    app.post('/user', async (req, res) => {
+      const newUser = req.body;
+      console.log('new user', newUser);
+      const result = await userEmailPass.insertOne(newUser);
+      res.send(result);
+    });
+
+    app.delete('/user/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
       const result = await userEmailPass.deleteOne(query);
       res.send(result);
-    })
-
-
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -114,11 +114,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-  res.send('Art and Craft Store!')
-})
+  res.send('Art and Craft Store!');
+});
 
 app.listen(port, () => {
-  console.log(`Art project is running on port ${port}`)
-})
+  console.log(`Art project is running on port ${port}`);
+});
